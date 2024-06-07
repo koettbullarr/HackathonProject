52d4618db487d344b0938f92cdaf2c72facea4db
Nurzhanat Zhussup <nurzhanat2002@gmail.com>
2024-06-07 17:14:26 +0200

    Update code

--- api_creation.py
+++ api_creation.py
@@ -1,6 +1,8 @@
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
+from torch.nn.functional import softmax
+from torch import argmax
+from torch import no_grad
 
 
 model_path = './model/fine-tuned-model'
@@ -14,12 +16,12 @@ def classify(text):
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
 
     # Make predictions
-    with torch.no_grad():
+    with no_grad():
         outputs = model(**inputs)
 
     logits = outputs.logits
-    prediction = ['fake' if x == 1 else 'real' for x in torch.argmax(logits, dim=-1).numpy()][0]
-    probabilities = torch.nn.functional.softmax(logits, dim=-1).cpu().numpy()[0]
+    prediction = ['fake' if x == 1 else 'real' for x in argmax(logits, dim=-1).numpy()][0]
+    probabilities = softmax(logits, dim=-1).cpu().numpy()[0]
 
     return prediction, probabilities
 
